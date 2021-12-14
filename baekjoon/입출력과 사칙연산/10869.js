4f9e7cea2b0673c95bfa38241c96d6493c1cfd28
nxnaxx <noname1260@naver.com>
2021-12-14 14:54:08 +0900

    Add 10869.js to baekjoon

---
+++ baekjoon/입출력과 사칙연산/10869.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let input = fs.readFileSync('test.txt').toString().split(' ');
+
+const a = Number(input[0]), b = Number(input[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
