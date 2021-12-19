fbe8d15bf3dc21c6f6320efd1934b5e5e721fa78
nxnaxx <noname1260@naver.com>
2021-12-19 22:07:30 +0900

    Add 11021.js to baekjoon

---
+++ baekjoon/for문/11021.js
@@ -0,0 +1,11 @@
+let input = require('fs').readFileSync('test.txt').toString().split('\n');
+
+const n = Number(input[0]);
+let result = '';
+
+for (let i = 1; i <= n; i++) {
+  let num = input[i].split(' ');
+  result += `Case #${i}: ${Number(num[0]) + Number(num[1]) + '\n'}`;
+}
+
+console.log(result);
