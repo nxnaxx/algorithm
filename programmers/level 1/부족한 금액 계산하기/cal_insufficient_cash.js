f42f80fed02553712d01414dd0913b104e1023a5
nxnaxx <76546167+nxnaxx@users.noreply.github.com>
2021-12-30 23:58:16 +0900

    Add cal_insufficient_cash.js to programmers

---
+++ programmers/level 1/부족한 금액 계산하기/cal_insufficient_cash.js
@@ -0,0 +1,8 @@
+function solution(price, money, count) {
+  for (let i = 1; i <= count; i++) {
+    money -= price * i;
+  }
+  return money < 0 ? Math.abs(money) : 0;
+}
+
+console.log(solution(3, 20, 4));
