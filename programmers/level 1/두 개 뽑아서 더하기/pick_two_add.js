cb26027e7737464793ff9bb0a2156d107f028523
nxnaxx <noname1260@naver.com>
2022-01-06 23:36:35 +0900

    Add pick_two_add.js to programmers

---
+++ programmers/level 1/두 개 뽑아서 더하기/pick_two_add.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  let arr = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      arr.push(numbers[i] + numbers[j]);
+    }
+  }
+  return Array.from(new Set(arr)).sort((a, b) => a - b);
+}
+
+console.log(solution([2, 1, 3, 4, 1]));
+console.log(solution([5, 0, 2, 7]));
