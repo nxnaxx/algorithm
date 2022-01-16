e92ca8e9f188d50160bf680bcdedd4bac9cc0879
nxnaxx <noname1260@naver.com>
2022-01-16 22:05:19 +0900

    Add make_decimal.js to programmers

---
+++ programmers/level 1/소수 만들기/make_decimal.js
@@ -0,0 +1,28 @@
+function solution(nums) {
+  let count = 0;
+
+  const isPrime = (num) => {
+    for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        //let input = nums[i] + nums[j] + nums[k];
+
+        if (isPrime(nums[i] + nums[j] + nums[k])) {
+          count++;
+        }
+      }
+    }
+  }
+  return count;
+}
+
+console.log(solution([1, 2, 3, 4]));
+console.log(solution([1, 2, 7, 6, 4]));
