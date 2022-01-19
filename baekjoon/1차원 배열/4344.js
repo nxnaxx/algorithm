2a2972c0bd4efde2ac6dbfee5fb149eede0db192
nxnaxx <noname1260@naver.com>
2022-01-19 23:14:33 +0900

    Add 4344.js to baekjoon

---
+++ baekjoon/1차원 배열/4344.js
@@ -0,0 +1,24 @@
+let input = require('fs').readFileSync('test.txt').toString().split('\n');
+
+const testCaseNum = Number(input[0]);
+
+for (let i = 1; i <= testCaseNum; i++) {
+  const testCase = input[i].split(' ').map((x) => Number(x));
+  const num = testCase[0];
+  let sum = 0;
+
+  for (let j = 1; j <= num; j++) {
+    sum += testCase[j];
+  }
+
+  //const average = sum / num;
+  testCase.splice(0, 1);
+  let topRanks = 0;
+
+  testCase.map((x) => {
+    if (x > sum / num) {
+      topRanks++;
+    }
+  });
+  console.log(`${((topRanks / num) * 100).toFixed(3)}%`);
+}
