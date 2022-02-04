1791e0d164dd949bda6a854d6e6ecd588774d927
nxnaxx <noname1260@naver.com>
2022-02-05 00:24:54 +0900

    Fix new_id.js in programmers

--- programmers/level 1/신규 아이디 추천/new_id.js
+++ programmers/level 1/신규 아이디 추천/new_id.js
@@ -1,42 +1,23 @@
 function solution(new_id) {
-  if (/[A-Z]/g.test(new_id)) {
-    new_id = new_id.toLowerCase();
-  }
+  let result = new_id
+    .toLowerCase()
+    .replace(/[^.\-_a-z0-9]/g, '')
+    .replace(/\.{2,}/g, '.')
+    .replace(/^[\.]|[\.]$/g, '')
+    .replace(/^$/, 'a')
+    .slice(0, 15)
+    .replace(/\.$/, '');
 
-  if (/[^(.\-_a-z0-9)]/g.test(new_id)) {
-    new_id = new_id.replace(/[^(.\-_a-z0-9)]/g, '');
-  }
-
-  if (/\.{2,}/g.test(new_id)) {
-    new_id = new_id.replace(/\.{2,}/g, '.');
-  }
-
-  if (new_id.charAt(0) === '.') {
-    new_id = new_id.replace(/^./, '');
-  } else if (new_id.charAt(new_id.length - 1) === '.') {
-    new_id = new_id.replace(/.$/, '');
-  }
-
-  if (new_id === '') {
-    new_id += 'a';
-  }
-
-  if (new_id.length >= 16) {
-    new_id = new_id.slice(0, 15);
-    if (new_id.charAt(new_id.length - 1) === '.') {
-      new_id = new_id.replace(/.$/, '');
+  if (result.length <= 2) {
+    let addStr = result.slice(-1);
+    for (let i = result.length; i < 3; i++) {
+      result += addStr;
     }
   }
 
-  if (new_id.length <= 2) {
-    let addStr = new_id.slice(-1);
-    for (let i = new_id.length; i < 3; i++) {
-      new_id += addStr;
-    }
-  }
-
-  return new_id;
+  return result;
 }
+
 console.log(solution('...!@BaT#*..y.abcdefghijklm'));
 console.log(solution('z-+.^.'));
 console.log(solution('=.='));
