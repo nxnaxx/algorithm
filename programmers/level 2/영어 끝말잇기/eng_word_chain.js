9a2657e22ad6ad8ab2f1588526c9c3dd0af8b4ee
nxnaxx <noname1260@naver.com>
2022-02-13 23:11:37 +0900

    Add eng_word_chain.js to programmers

---
+++ programmers/level 2/영어 끝말잇기/eng_word_chain.js
@@ -0,0 +1,35 @@
+function solution(n, words) {
+  let result = [0, 0];
+  for (let i = 1; i < words.length; i++) {
+    if (words[i][0] !== words[i - 1][words[i - 1].length - 1] || words.indexOf(words[i]) !== i) {
+      result[0] = Math.floor(i % n) + 1;
+      result[1] = Math.floor(i / n) + 1;
+      break;
+    }
+  }
+  return result;
+}
+
+console.log(
+  solution(3, ['tank', 'kick', 'know', 'wheel', 'land', 'dream', 'mother', 'robot', 'tank'])
+);
+console.log(
+  solution(5, [
+    'hello',
+    'observe',
+    'effect',
+    'take',
+    'either',
+    'recognize',
+    'encourage',
+    'ensure',
+    'establish',
+    'hang',
+    'gather',
+    'refer',
+    'reference',
+    'estimate',
+    'executive',
+  ])
+);
+console.log(solution(2, ['hello', 'one', 'even', 'never', 'now', 'world', 'draw']));
