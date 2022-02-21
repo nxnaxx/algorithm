0a2cbec75139eb94ed1e4fb8d74c420df31d2731
nxnaxx <noname1260@naver.com>
2022-02-21 23:44:11 +0900

    Add H_index.js to programmers

---
+++ programmers/level 2/H-Index/H_index.js
@@ -0,0 +1,11 @@
+function solution(citations) {
+  citations.sort((a, b) => b - a);
+
+  let index = 0;
+  while (index + 1 <= citations[index]) {
+    index++;
+  }
+
+  return index;
+}
+console.log(solution([3, 0, 6, 1, 5]));
