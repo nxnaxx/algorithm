83b2707490f86707b970595a33cfe416554ba5b5
nxnaxx <noname1260@naver.com>
2022-02-23 23:33:45 +0900

    Add remove_pairs.js to programmers

---
+++ programmers/level 2/짝지어 제거하기/remove_pairs.js
@@ -0,0 +1,22 @@
+function solution(s) {
+  const arr = s.split('');
+  const result = [];
+
+  result.push(arr[0]);
+
+  for (let i = 1; i < arr.length; i++) {
+    if (result === '') {
+      result.push(arr[i]);
+      continue;
+    }
+
+    if (result[result.length - 1] === arr[i]) {
+      result.pop();
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result == '' ? 1 : 0;
+}
+console.log(solution('baabaa'));
+console.log(solution('cdcd'));
