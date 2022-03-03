acd4990e0810424be18e4d430c8135c2c92ebd5d
nxnaxx <noname1260@naver.com>
2022-03-03 23:21:15 +0900

    Add str_compression.js to programmers

---
+++ programmers/level 2/문자열 압축/str_compression.js
@@ -0,0 +1,27 @@
+function solution(s) {
+  let result = s.length;
+  for (let i = 1; i <= Math.floor(s.length / 2); i++) {
+    let sample = s.substr(0, i);
+    let count = 1;
+    let str = '';
+
+    for (let j = i; j < s.length; j += i) {
+      if (sample === s.substr(j, i)) {
+        count++;
+      } else {
+        str += count === 1 ? sample : count + sample;
+        count = 1;
+        sample = s.substr(j, i);
+      }
+    }
+
+    str += count === 1 ? sample : count + sample;
+    result = Math.min(result, str.length);
+  }
+  return result;
+}
+console.log(solution('aabbaccc'));
+console.log(solution('ababcdcdababcdcd'));
+console.log(solution('abcabcdede'));
+console.log(solution('abcabcabcabcdededededede'));
+console.log(solution('xababcdcdababcdcd'));
