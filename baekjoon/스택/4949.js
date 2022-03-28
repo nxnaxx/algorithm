da7aeaf8642ba2c0c6e2d8185024bb53998c5e97
nxnaxx <noname1260@naver.com>
2022-03-28 16:54:50 +0900

    Modify 4949.js in baekjoon

--- baekjoon/스택/4949.js
+++ baekjoon/스택/4949.js
@@ -1,12 +1,10 @@
 let input = require('fs').readFileSync('test.txt').toString().trim().split('\n').slice(0, -1);
 
-//const removeStr = /[a-zA-Z ]/g;
 let result = '';
 
 input.map((x) => {
   let checkStr = x.replace(/[a-zA-Z \.\r]/g, '');
   let stack = [];
-  //console.log(checkStr);
 
   for (let i = 0; i < checkStr.length; i++) {
     if (checkStr[i] === ')' && stack[stack.length - 1] === '(') {
@@ -19,10 +17,6 @@ input.map((x) => {
     stack.push(checkStr[i]);
   }
 
-  console.log(stack);
-
-  //let result = stack.length <= 1 ? 'yes' : 'no';
-  //console.log(result);
   result += stack.length === 0 ? `yes\n` : `no\n`;
 });
 
