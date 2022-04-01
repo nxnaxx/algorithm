74b3202fbc0904db443acc294b410f50f55766c7
nxnaxx <noname1260@naver.com>
2022-04-02 00:31:00 +0900

    Add truck_pass_bridge.js to programmers

---
+++ programmers/level 2/다리를 지나는 트럭/truck_pass_bridge.js
@@ -0,0 +1,28 @@
+function solution(bridge_length, weight, truck_weights) {
+  const onBridge = [];
+  let count = 0;
+  //let result = 0;
+  let totalWeight = 0;
+
+  while (truck_weights.length > 0) {
+    count++;
+    if (onBridge.length === bridge_length) {
+      totalWeight -= onBridge.shift();
+    }
+    if (totalWeight + truck_weights[0] > weight) {
+      onBridge.push(0);
+      continue;
+    }
+    let currTruck = truck_weights.shift();
+    onBridge.push(currTruck);
+    totalWeight += currTruck;
+  }
+
+  return (count += 2);
+}
+
+/*else if ((onBridge.length < bridge_length) && (total + truck_weights[i] > weight)) {
+      onBridge.shift();
+ } */
+
+console.log(solution(2, 10, [7, 4, 5, 6]));
