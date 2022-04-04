0631d303c1696c0fb6a602b87aea09629acc0abb
nxnaxx <noname1260@naver.com>
2022-04-04 23:50:24 +0900

    Add vowel_dict.js to baekjoon

---
+++ programmers/level 2/모음사전/vowel_dict.js
@@ -0,0 +1,23 @@
+function solution(word) {
+  const vowels = {
+    A: 0,
+    E: 1,
+    I: 2,
+    O: 3,
+    U: 4,
+  };
+
+  const arr = [781, 156, 31, 6, 1];
+  let result = 0;
+
+  word.split('').map((x, i) => {
+    result += vowels[x] * arr[i] + 1;
+  });
+
+  return result;
+}
+
+console.log(solution('AAAAE'));
+console.log(solution('AAAE'));
+console.log(solution('I'));
+console.log(solution('EIO'));
