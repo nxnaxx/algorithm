7afb20f00822722219a276625086eb836d79a8e3
nxnaxx <noname1260@naver.com>
2022-04-14 21:47:47 +0900

    Add prediction_table.js to programmers

---
+++ programmers/level 2/예상 대진표/prediction_table.js
@@ -0,0 +1,12 @@
+function solution(n, a, b) {
+  let count = 0;
+  const nextNum = (n) => Math.floor((n + 1) / 2);
+  while (a !== b) {
+    a = nextNum(a);
+    b = nextNum(b);
+    count++;
+  }
+  return count;
+}
+
+console.log(solution(8, 4, 7));
